ed243899f59bbface2e09f6a6705ce123b28b394
Mehmet Taha <103601089+Honey171@users.noreply.github.com>
2023-02-02 21:57:41 +0300

    Update Wrapper.tsx

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -8,7 +8,7 @@ function Wrapper() {
   const loading = useAppSelector((state) => state.loading);
 
   return (
-    <section className="bg-[#050A30] lg:w-[62rem] mx-auto flex space-y-8 flex-col items-center justify-center lg:h-[45rem] rounded-xl py-5 px-5 md:px-10 lg:py-0 lg:px-0">
+    <section className="bg-[#050A30]/70 lg:w-[62rem] mx-auto flex space-y-8 flex-col items-center justify-center lg:h-[45rem] rounded-xl py-5 px-5 md:px-10 lg:py-0 lg:px-0">
       {loading ? (
         <div className="text-white">
           <Spinner /> Loading city data...
